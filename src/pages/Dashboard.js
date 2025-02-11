0a19375d5236d119a230599225502c1930bb1427
andrewolsen <a.olsen1110@gmail.com>
2025-02-10 21:25:49 -0600

    Add debounce for filter

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import DashboardLayout from '../components/DashboardLayout';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Outlet } from 'react-router';
 
 const dummyMeals = [
@@ -9,13 +9,23 @@ const dummyMeals = [
 ];
 
 function MealTable({ dummyMeals }) {
-  const [tableFilter, setFilter] = useState(200);
+  const [inputValue, setInputValue] = useState('');
+  const [tableFilter, setTableFilter] = useState('');
 
   const handleFilter = (e) => {
-    if (!isNaN(e.target.value)) {
-      setFilter(e.target.value);
-    }
+    setInputValue(e.target.value);
   };
+
+  // use debounce so tableFilter doesn't update after every keystroke
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (inputValue === '' || !isNaN(inputValue)) {
+        setTableFilter(inputValue === '' ? '' : Number(inputValue));
+      }
+      console.log(inputValue);
+    }, 500);
+    return () => clearTimeout(handler);
+  }, [inputValue]);
 
   const filteredMeals = dummyMeals.filter(
     (meal) => meal.calories >= tableFilter
@@ -27,31 +37,35 @@ function MealTable({ dummyMeals }) {
         <input
           type='text'
           placeholder='Set filter'
-          value={tableFilter}
+          value={inputValue}
           onChange={handleFilter}
         />
       </div>
       <table>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Calories (Per Serving)</th>
-          <th>Protein (Per Serving)</th>
-          <th>Servings</th>
-          <th>Total Calories</th>
-          <th>Total Protein</th>
-        </tr>
-        {filteredMeals.map((meal) => (
+        <thead>
           <tr>
-            <td>{meal.id}</td>
-            <td>{meal.name}</td>
-            <td>{meal.calories}</td>
-            <td>{meal.protein}</td>
-            <td>{meal.servings}</td>
-            <td>{meal.calories * meal.servings}</td>
-            <td>{meal.protein * meal.servings}</td>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Calories (Per Serving)</th>
+            <th>Protein (Per Serving)</th>
+            <th>Servings</th>
+            <th>Total Calories</th>
+            <th>Total Protein</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {filteredMeals.map((meal) => (
+            <tr key={meal.id}>
+              <td>{meal.id}</td>
+              <td>{meal.name}</td>
+              <td>{meal.calories}</td>
+              <td>{meal.protein}</td>
+              <td>{meal.servings}</td>
+              <td>{meal.calories * meal.servings}</td>
+              <td>{meal.protein * meal.servings}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );

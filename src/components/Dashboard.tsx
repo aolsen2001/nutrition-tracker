42c43742dddfd2ac1df4b7bd014e82d34a5ac362
andrewolsen <a.olsen1110@gmail.com>
2025-02-17 13:05:37 -0600

    Initial commit

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,67 @@
+// import { useState } from "react";
+import { Meal } from "../types";
+
+function MealList(meals: Meal[]) {
+  const mealListItems = meals.map((meal: Meal) => (
+    <li key={meal.id}>
+      <p>
+        {meal.name} - {meal.calories} - {meal.protein} - {meal.servings} -
+        {meal.date.toLocaleString().split(",")[0]} -
+        {meal.date.toLocaleString().split(",")[1]}
+      </p>
+    </li>
+  ));
+
+  return mealListItems;
+}
+
+function Dashboard() {
+  const meals: Meal[] = [
+    {
+      id: 1,
+      name: "Chicken Breast",
+      calories: 300,
+      protein: 30,
+      servings: 1,
+      date: new Date(),
+    },
+    {
+      id: 2,
+      name: "Greek Yogurt",
+      calories: 200,
+      protein: 15,
+      servings: 2,
+      date: new Date(),
+    },
+    {
+      id: 3,
+      name: "Cheeseburger",
+      calories: 500,
+      protein: 22,
+      servings: 1,
+      date: new Date(),
+    },
+    {
+      id: 4,
+      name: "Mixed Nuts",
+      calories: 200,
+      protein: 12,
+      servings: 3,
+      date: new Date(),
+    },
+    {
+      id: 5,
+      name: "Protein Shake",
+      calories: 400,
+      protein: 40,
+      servings: 1,
+      date: new Date(),
+    },
+  ];
+
+  const mealList = MealList(meals);
+
+  return <ul>{mealList}</ul>;
+}
+
+export default Dashboard;
---
+++ src/types.tsx
@@ -0,0 +1,8 @@
+export type Meal = {
+  id: number;
+  name: string;
+  calories: number;
+  protein: number;
+  servings: number;
+  date: Date;
+};

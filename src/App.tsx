4918f70aefd8066e1b519f7c62c7a270b2e20373
andrewolsen <a.olsen1110@gmail.com>
2025-04-08 20:38:08 -0500

    Refactor code to utilize App.tsx as the entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import Dashboard from './components/Dashboard';
-// import './App.css';
+import { StrictMode, useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { auth } from './firebase.tsx';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SignUp from './SignUp.tsx';
+import Login from './Login.tsx';
+import Dashboard from './components/Dashboard.tsx';
+import DashboardLayout from './components/DashboardLayout.tsx';
+import Landing from './components/Landing.tsx';
+import LogMeal from './components/LogMeal.tsx';
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const queryClient = new QueryClient();
+
   return (
-    <>
-      <Dashboard />
-    </>
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/dashboard' element={<DashboardLayout user={user} />}>
+            <Route index element={<Dashboard />} />
+            <Route
+              path='/dashboard/logmeal'
+              element={
+                <QueryClientProvider client={queryClient}>
+                  <LogMeal />
+                </QueryClientProvider>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 }
 
